24215059eac6ead481a7a69503eebda5f93872f7
saulgoodmanvenus-dot <saulgoodman.venus@gmail.com>
2026-05-01 23:03:41 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -120,7 +120,7 @@ export default function Hero() {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-5xl mx-auto px-6 text-center">
+      <div className="relative z-10 max-w-5xl mx-auto px-6 text-center pt-28 md:pt-16 lg:pt-0">
         {/* Badge */}
         <motion.div
           initial={{ opacity: 0, y: 30 }}
